466fdb44d9c1d020b7c92b732b5d746cbba222cc
Kathiravan-Elangovan <79823704+Kathiravan-Elangovan@users.noreply.github.com>
2021-05-28 14:37:08 +0530

    Add files via upload

---
+++ jQuery/AddNumbers/AddNumbers.js
@@ -0,0 +1,16 @@
+function add_two_number()
+{
+   $("#display_message").show();
+   if($("#txt1").val()=="" || $("#txt2").val()=="")
+   {
+      $("#display_message").html("<font color='red'>Pls. Enter The Values.</font>");
+   }
+   else
+  {
+    var answer=parseInt($("#txt1").val()) + parseInt($("#txt2").val())
+       
+    $("#display_message").html("<font color='green'><b>"+answer+"</b></font>");
+ $("#txt3").val(answer);
+ 
+   }
+}
