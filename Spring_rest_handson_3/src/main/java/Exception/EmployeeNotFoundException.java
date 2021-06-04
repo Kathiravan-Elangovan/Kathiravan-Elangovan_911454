44ec61970fbc5568dec7596ce44e903dd44c2e05
Kathiravan-Elangovan <79823704+Kathiravan-Elangovan@users.noreply.github.com>
2021-06-04 12:10:40 +0530

    Delete Spring_rest_handson_3 directory

--- Spring_rest_handson_3/src/main/java/Exception/EmployeeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package Exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST,reason = "Employee Not Found")
-public class EmployeeNotFoundException extends Exception{
-
-}
